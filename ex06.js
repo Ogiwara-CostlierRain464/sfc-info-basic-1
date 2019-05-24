1fbfae9a300649022a341f60bbbe18c01dd95737
ogiwara <yushiogiwara@gmail.com>
2019-05-24 09:52:11 +0900

    complete 6

--- ex06.js
+++ ex06.js
@@ -1,7 +1,21 @@
-function sayhello() { alert('Hello, world!');
+const PHOTO_1 = "photo1.png";
+const PHOTO_2 = "photo2.png";
+
+let currentPhoto = "photo1.png";
+
+/**
+ * 写真のスワップを行う
+ */
+function swapPhoto() {
+  const next = nextPhoto(currentPhoto);
+  currentPhoto = next;
+
+  document.getElementById("photo").innerHTML = "<img alt='photo1' src='" + next + "' />";
 }
 
-// typeだったので直しておきました
-function sayGoodBye(){
-  alert("Goodbye.");
+/**
+ * 次の写真を取得
+ */
+function nextPhoto(current) {
+  return current === PHOTO_1 ? PHOTO_2 : PHOTO_1;
 }
